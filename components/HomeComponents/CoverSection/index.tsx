26cfc53e8570e590343ac346b04988d7319abb5f
Barilelo <miikorbarilelo@gmail.com>
2023-03-13 19:10:07 +0100

    Update index.tsx

--- components/HomeComponents/CoverSection/index.tsx
+++ components/HomeComponents/CoverSection/index.tsx
@@ -3,8 +3,8 @@ import { Button } from "../../FormElements/Button";
 
 export function FrontSection() {
   return (
-    <section className="md2:h-[1026px] pt-[56px] xl:pt-[150px] text-center md2:text-left lg:pt-[50px] relative md2:flex items-center px-[16px] lg:px-[108px] md2:overflow-hidden">
-      <div style={{ flex: 0.5 }}>
+    <section className="md2:h-[1026px] pt-[56px] text-center md2:text-left lg:pt-[50px] relative md2:flex  px-[16px] lg:px-[108px] md2:overflow-hidden">
+      <div style={{ flex: 0.5 }} className="xl:mt-[50px]">
         <h1 className="font-semibold text-[40px] md2:text-[50px] lg:text-[60px]">
           Kickstart your career in any field of your choice
         </h1>
@@ -15,7 +15,7 @@ export function FrontSection() {
         <p className="font-semibold text-[28px] mt-[32px] md2:mt-[14px]">
           Take your first course on us
         </p>
-        <div className="absolute w-[100%] lg:w-[60%] xl:left-0 xl:flex-row z-20 bottom-[60px] hidden md:flex md2:block md2:w-fit md2:right-0 md2:flex md2:flex-col md2:items-center md2:gap-y-[20px] justify-between items-center px-[50px]">
+        <div className="absolute w-[100%] md2:w-[70%] xl:w-[60%] xl:left-0 xl:flex-row z-20 bottom-[60px] xl:bottom-[150px] hidden gap-x-[50px] md:flex  justify-between items-center px-[50px]">
           <Button
             text="Join for free"
             paddingBlock="18px"
@@ -40,7 +40,7 @@ export function FrontSection() {
         </div>
       </div>
       <div
-        className="w-full md2:w-fit md2:absolute md2:right-[-200px] mt-[23px] right-[-50px] xl:right-[25px] xl:bottom-[-100px] w-[595px]"
+        className="w-full md2:w-fit md2:absolute md2:right-[-200px] mt-[23px] right-[-50px] xl:right-[25px] xl:bottom-[0] w-[595px]"
         style={{ zIndex: 5 }}
       >
         <img src="/images/study-girl.svg" alt="model" className="mx-auto"></img>
@@ -59,7 +59,7 @@ export function FrontSection() {
         </div>
       </div>
       <div
-        className="w-[1150px] h-[1000px] rounded-[50%] absolute hidden xl:grid place-items-center"
+        className="w-[1200px] h-[1050px] rounded-[50%] absolute hidden xl:grid place-items-center top-[-30px]"
         style={{
           transform: "rotate(-72.9deg)",
           right: "-450px",
@@ -68,7 +68,7 @@ export function FrontSection() {
       >
         {" "}
         <div
-          className="w-[1100px] h-[950px] bg-white rounded-[50%] absolute"
+          className="w-[1150px] h-[1000px] bg-white rounded-[50%] absolute"
           style={{ backgroundColor: "#004AAD" }}
         ></div>
       </div>
