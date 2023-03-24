c63e032be540b608b6d083fc1bce7960882e015f
Barilelo <miikorbarilelo@gmail.com>
2023-03-24 14:03:15 +0100

    Finished the course content

--- components/CoursesComponents/ContentHeading/index.tsx
+++ components/CoursesComponents/ContentHeading/index.tsx
@@ -1,38 +1,65 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 interface ContentHeadingProps {
-  isActive: boolean;
-  setIsActive: Dispatch<SetStateAction<boolean>>;
+  head: string;
+  opened: string[];
+  setOpened: Dispatch<SetStateAction<string[]>>;
+  allOpen: boolean;
 }
 
-export function ContentHeading({ isActive, setIsActive }: ContentHeadingProps) {
-  const [hightlight, setHighlight] = useState<boolean>(true);
+export function ContentHeading({
+  head,
+  opened,
+  setOpened,
+  allOpen,
+}: ContentHeadingProps) {
+  const [hightlight, setHighlight] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkAllOpen = () => {
+      if (allOpen === true) {
+        setHighlight(true);
+      }
+    };
+    checkAllOpen();
+  }, [opened]);
+
+  const setOpenedArray = (title: string) => {
+    setOpened((prevVal) => [...prevVal, title]);
+    return;
+  };
 
   return (
     <div
       className="py-[18px] px-[28px] rounded-[8px] flex items-center justify-between cursor-pointer"
       style={
-        hightlight
+        hightlight === true && opened.includes(head)
           ? { backgroundColor: "#004AAD", color: "#fff" }
           : { backgroundColor: "#D2DCE8" }
       }
       onClick={(e) => {
         setHighlight((val) => !val);
+        if (opened.includes(head)) {
+          const headIndex: number = opened.indexOf(head);
+          setOpened((prevArray) =>
+            prevArray.filter((_, index) => index !== headIndex)
+          );
+        } else {
+          setOpenedArray(head);
+        }
       }}
     >
       <div className="flex gap-x-[19px]">
         <img
           src={
-            hightlight
+            hightlight === true && opened.includes(head)
               ? "/images/arrow-down-white.svg"
               : "/images/arrow-right-black.svg"
           }
           alt="arrow-right"
         ></img>
-        <p className="font-medium text-[20px]">
-          Introduction, Project & Course Material
-        </p>
+        <p className="font-medium text-[20px]">{head}</p>
       </div>
       <ul className={styles.contentSummary}>
         <li>3 Lectures </li>
--- components/CoursesComponents/CourseContent/index.tsx
+++ components/CoursesComponents/CourseContent/index.tsx
@@ -4,8 +4,20 @@ import { MainContent } from "../MainContents";
 import styles from "./index.module.css";
 
 export function CourseContent() {
-  const [isActive, setIsActive] = useState<boolean>(true);
-  const [openFields, setOpenFields] = useState<number>(100);
+  const headings: string[] = [
+    "introduction",
+    "fundamentals",
+    "setting",
+    "project",
+    "defining",
+    "chart",
+    "extra",
+    "before",
+    "what",
+  ];
+
+  const [opened, setOpened] = useState<string[]>([]);
+  const [allOpen, setAllOpen] = useState<boolean>(false);
 
   return (
     <section className="mt-[230px] px-[28px] xl:px-[95px]">
@@ -16,20 +28,36 @@ export function CourseContent() {
           <li className="font-medium text-[20px]">59 Lectures</li>
           <li className="font-medium text-[20px]">9h 4m Total Length</li>
         </ul>
-        <p className="font-medium text-[20px] text-mine">Expand All Sections</p>
+        <p
+          className="font-medium text-[20px] text-mine cursor-pointer"
+          onClick={() => {
+            setOpened((prevArray) => {
+              return [...headings];
+            });
+            setAllOpen(true);
+          }}
+        >
+          Expand All Sections
+        </p>
       </div>
       <div className="mt-[39px] flex flex-col gap-y-[35px]">
-        {[1, 1, 1, 1, 1].map((item, index) => {
+        {headings.map((item, index) => {
           return (
             <div>
-              <ContentHeading isActive={isActive} setIsActive={setIsActive} />
-              {
+              <ContentHeading
+                head={item}
+                opened={opened}
+                setOpened={setOpened}
+                allOpen={allOpen}
+                key={index}
+              />
+              {opened.includes(item) && (
                 <div>
                   {[1, 1, 1].map((item, index) => {
-                    return <MainContent documentSize="1.9KB" />;
+                    return <MainContent documentSize="1.9KB" key={index} />;
                   })}
                 </div>
-              }
+              )}
             </div>
           );
         })}
