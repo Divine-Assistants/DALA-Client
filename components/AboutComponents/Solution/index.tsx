8d5358a473aaca0cf22d94cc68a3c68628a01ade
Barilelo <miikorbarilelo@gmail.com>
2023-03-30 14:11:47 +0100

    Update index.tsx

--- components/AboutComponents/Solution/index.tsx
+++ components/AboutComponents/Solution/index.tsx
@@ -1,42 +1,50 @@
-import React from 'react'
+import React from "react";
 
 export function Solution() {
   return (
     <>
-                        <div className='bg-[url("/images/about-back.svg")] bg-center mb-[271px] '>
-            <div className=' pt-[80px] w-[90%] m-auto text-[#FAFAFA] lg:flex lg:gap-[83px] lg:py-[155px] '>
-                        <div className='lg:w-[40%] '>
-                          <h2 className='font- semibold text-[24px] mb-[5px] md:text-[32px] lg:text-[50px] '>The Problem</h2>  
-                          <p className='text-[16px] mb-[40px] md:text-[24px]  '>In my experience as a young person growing up in Africa, one thing has
-been constant: there are never enough jobs available for the kids of the
-neighbourhood. Many families have toiled to put their children through
-elementary, secondary, and tertiary education, and yet even after all of
-this, there are never enough jobs for the young. In nations like Nigeria,
-where young people are struggling to find work, we have seen a
-dramatic surge in fraud. Few retirees among the elderly have sufficient
-pension money to support their desire to do so, and those who do are
-unlikely to find work in their later years. For a very long period, this has
-been a consistent situation throughout Africa.</p>
-                        </div>
-                <div className='flex items-center gap-[23px] mb-[40px] lg:flex-col lg:gap[23px] lg:w-[5%]  '>
-                  <div className='w-[40%] lg:w-[4px] lg:bg-white lg:h-[40%] border-2 border-[#FAFAFA] '></div>
-                  <div className='w-[10%] lg:w-[4px] lg:bg-white lg:h-[10%] border-2 border-[#FAFAFA]'></div>
-                  <div className='w-[40%] lg:w-[4px] lg:bg-white lg:h-[40%] border-2 border-[#FAFAFA]'></div>
-                </div>
-                  <div className='lg:w-[40%] lg:mt-[0px] '>
-                        <h2 className='font- semibold text-[24px] mb-[5px] lg:text-[50px] md:text-[32px] '>Our Solution</h2>  
-                  <p className='text-[16px] pb-[72px] lg:text-[24px] md:text-[24px]  '>We like to call our solution operation
-"Bringing the Dollar Back Home."
-The DivineAssistants Learning Academy is going to focus on providing
-video-based learning programs for individuals in Africa.
-DivineAssistants itself is a digital firm for business management.
-DivineAssistants is a virtual support company that is dedicated to
-bringing entrepreneurs virtual team services to help them handle their
-day-to-day tasks remotely while they focus on handling their core
-business growth.</p>
-                        </div>
-            </div>
-            </div>
+      <div className='bg-[url("/images/about-back.svg")] bg-center mb-[271px] '>
+        <div className=" pt-[80px] w-[90%] m-auto text-[#FAFAFA] lg:flex lg:gap-[83px] lg:py-[155px] ">
+          <div className="lg:w-[40%] ">
+            <h2 className="font- semibold text-[24px] mb-[5px] md:text-[32px] lg:text-[50px] ">
+              The Problem
+            </h2>
+            <p className="text-[16px] mb-[40px] md:text-[24px]  ">
+              In my experience as a young person growing up in Africa, one thing
+              has been constant: there are never enough jobs available for the
+              kids of the neighbourhood. Many families have toiled to put their
+              children through elementary, secondary, and tertiary education,
+              and yet even after all of this, there are never enough jobs for
+              the young. In nations like Nigeria, where young people are
+              struggling to find work, we have seen a dramatic surge in fraud.
+              Few retirees among the elderly have sufficient pension money to
+              support their desire to do so, and those who do are unlikely to
+              find work in their later years. For a very long period, this has
+              been a consistent situation throughout Africa.
+            </p>
+          </div>
+          <div className="flex items-center gap-[23px] mb-[40px] lg:flex-col lg:gap[23px] lg:w-[5%]  ">
+            <div className="w-[40%] lg:w-[4px] lg:bg-white lg:h-[40%] border-2 border-[#FAFAFA] "></div>
+            <div className="w-[10%] lg:w-[4px] lg:bg-white lg:h-[10%] border-2 border-[#FAFAFA]"></div>
+            <div className="w-[40%] lg:w-[4px] lg:bg-white lg:h-[40%] border-2 border-[#FAFAFA]"></div>
+          </div>
+          <div className="lg:w-[40%] lg:mt-[0px] ">
+            <h2 className="font- semibold text-[24px] mb-[5px] lg:text-[50px] md:text-[32px] ">
+              Our Solution
+            </h2>
+            <p className="text-[16px] pb-[72px] lg:text-[24px] md:text-[24px]  ">
+              We like to call our solution operation Bringing the Dollar Back
+              Home. The DivineAssistants Learning Academy is going to focus on
+              providing video-based learning programs for individuals in Africa.
+              DivineAssistants itself is a digital firm for business management.
+              DivineAssistants is a virtual support company that is dedicated to
+              bringing entrepreneurs virtual team services to help them handle
+              their day-to-day tasks remotely while they focus on handling their
+              core business growth.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
